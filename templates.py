28b736e499c7e80abe417ca40b52f372bf1813b5
Mateus <mhlbueno@hotmail.com>
2024-11-27 21:17:46 +0000

    Atividade Semana 090

--- templates.py
+++ templates.py
@@ -1,28 +1,19 @@
-# A very simple Flask Hello World app for you to get started with...
-
 import os
-
-from flask import Flask, render_template, session, redirect, url_for, flash, request
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_migrate import Migrate
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
-
-from datetime import datetime
-
-class NameForm(FlaskForm):
-  name = StringField('What is your name?', validators= [DataRequired()])
-  submit = SubmitField('Submit')
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Chave forte'
+app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-  'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
@@ -30,33 +21,64 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Role(db.Model):
-  __tablename__ = 'roles'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(64), unique=True)
-  users = db.relationship('User', backref='role')
 
-  def __repr__(self):
-    return '<Role %r>' % self.name
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
 class User(db.Model):
-  __tablename__ = 'users'
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(64), unique=True, index=True)
-  role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-  def __repr__(self):
-    return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField('Role?', choices=[('Administrator', 'Administrator'), ('Moderator', 'Moderator'), ('User', 'User')])
+    submit = SubmitField('Submit')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+
+    users = User.query.all()
+    roles = Role.query.all()
+
+    users_len = len(users)
+    roles_len = len(roles)
+
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            role = Role.query.filter_by(name=form.role.data).first()
+            user = User(username=form.name.data, role=role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -65,25 +87,5 @@ def index():
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
-
-
-@app.route('/user/<name>/<id>/<institution>')
-def name(name, id, institution):
-    return render_template('user.html', name = name, id = id, institution = institution)
-
-@app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    return render_template('contextorequisicao.html', name = name, user_agent = request.headers.get('User-Agent'), user_ip = request.remote_addr, app_host = request.host)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
- return render_template('500.html'), 500
-
-@app.shell_context_processor
-def make_shell_context():
-  return dict(db=db, User=User, Role=Role)
+                           known=session.get('known', False), users=users, users_len=users_len,
+                           roles=roles, roles_len=roles_len)
